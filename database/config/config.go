1d5f1c0d630bb1c9aa193f30f82e88da94f9831c
Akizon77 <i@akz.moe>
2025-04-28 20:37:34 +0800

    feat: 前端嵌入二进制

--- database/config/config.go
+++ database/config/config.go
@@ -3,12 +3,21 @@ package config
 import (
 	"github.com/komari-monitor/komari/database/dbcore"
 	"github.com/komari-monitor/komari/database/models"
+	"github.com/komari-monitor/komari/public"
+	"gorm.io/gorm"
 )
 
 func Get() (models.Config, error) {
 	db := dbcore.GetDBInstance()
 	var config models.Config
 	if err := db.First(&config).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			config = models.Config{
+				ID: 1,
+			}
+			Save(config)
+			return config, nil
+		}
 		return config, err
 	}
 	return config, nil
@@ -22,5 +31,6 @@ func Save(cst models.Config) error {
 	if err := db.Save(&config).Error; err != nil {
 		return err
 	}
+	go public.UpdateIndex(config)
 	return nil
 }
