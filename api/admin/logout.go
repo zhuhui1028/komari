5accc9d258021e91360513b3cb166cdd148954f1
Akizon77 <i@akz.moe>
2025-05-12 16:12:20 +0800

    feat: #24 添加logout登出

---
+++ api/admin/logout.go
@@ -0,0 +1,13 @@
+package admin
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/komari-monitor/komari/database/accounts"
+)
+
+func Logout(c *gin.Context) {
+	session, _ := c.Cookie("session_token")
+	accounts.DeleteSession(session)
+	c.SetCookie("session_token", "", -1, "/", "", false, true)
+	c.Redirect(302, "/")
+}
