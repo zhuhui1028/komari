5316a4d3b211e32fb5e41860a80f9942e32cd9f1
Akizon77 <i@akz.moe>
2025-05-15 20:49:12 +0800

    feat: 添加获取客户端最近记录的API接口

--- cmd/server.go
+++ cmd/server.go
@@ -76,6 +76,7 @@ var ServerCmd = &cobra.Command{
 		r.GET("/api/oauth", api.OAuth)
 		r.GET("/api/oauth_callback", api.OAuthCallback)
 		r.GET("/api/logout", api.Logout)
+		r.GET("/api/recent/:uuid", api.GetClientRecentRecords)
 
 		tokenAuthrized := r.Group("/api/clients", api.TokenAuthMiddleware())
 		{
