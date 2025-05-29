95cf1d78d0600c3127e0ce72be9f0f29da2572bf
Akizon77 <i@akz.moe>
2025-05-29 17:51:04 +0800

    feat: 添加SSO相关信息返回

--- api/me.go
+++ api/me.go
@@ -22,6 +22,6 @@ func GetMe(c *gin.Context) {
 		c.JSON(200, gin.H{"username": "Guest", "logged_in": false})
 		return
 	}
-	c.JSON(200, gin.H{"username": user.Username, "logged_in": true, "uuid": user.UUID})
+	c.JSON(200, gin.H{"username": user.Username, "logged_in": true, "uuid": user.UUID, "sso_type": user.SSOType, "sso_id": user.SSOID})
 
 }
