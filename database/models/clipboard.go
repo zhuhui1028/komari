4863e623ae735d9965d84e7f14631065055b8b1d
Akizon77 <i@akz.moe>
2025-06-13 22:14:58 +0800

    wip: 命令剪切板

---
+++ database/models/clipboard.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Clipboard struct {
+	Id        int       `json:"id" gorm:"primaryKey;autoIncrement;unique"`
+	Text      string    `json:"text" gorm:"type:longtext"`
+	Name      string    `json:"name" gorm:"type:varchar(255)"`
+	Remark    string    `json:"remark" gorm:"type:text"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
