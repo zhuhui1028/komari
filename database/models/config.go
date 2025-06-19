65cf7b30c375493f7440c645afb6c6542d41e9a2
Akizon77 <i@akz.moe>
2025-06-19 14:31:03 +0800

    wip: Ping、TCPing、HTTPing

--- database/models/config.go
+++ database/models/config.go
@@ -25,6 +25,9 @@ type Config struct {
 	TelegramChatID   string `json:"telegram_chat_id" gorm:"type:varchar(255)"`
 	// 通知
 	NotificationEnabled bool `json:"notification_enabled" gorm:"default:false"` // 通知总开关
-	CreatedAt           time.Time
-	UpdatedAt           time.Time
+	// Records
+	RecordEnabled      bool `json:"record_enabled" gorm:"default:true"`      // 是否启用记录功能
+	RecordPreserveTime int  `json:"record_preserve_time" gorm:"default:720"` // 记录保留时间，单位小时，默认30天
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
 }
