a95d621bdbccefed0aecf06f9f8a131f15970fee
Akizon77 <i@akz.moe>
2025-06-22 21:53:19 +0800

    feat: 不再自动清除ping记录，重命名Telegram测试为发送消息

--- cmd/server.go
+++ cmd/server.go
@@ -120,8 +120,8 @@ var ServerCmd = &cobra.Command{
 			// test
 			testGroup := adminAuthrized.Group("/test")
 			{
-				testGroup.GET("/telegram", test.TestTelegram)
 				testGroup.GET("/geoip", test.TestGeoIp)
+				testGroup.POST("/sendMessage", test.TestSendMessage)
 			}
 			// update
 			updateGroup := adminAuthrized.Group("/update")
