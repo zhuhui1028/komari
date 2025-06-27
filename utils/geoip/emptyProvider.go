c09bec5c201eaae72936a2605297aae7eadfc954
Akizon77 <i@akz.moe>
2025-06-27 21:05:59 +0800

    refactor: 更新GeoIP服务实现

---
+++ utils/geoip/emptyProvider.go
@@ -0,0 +1,21 @@
+package geoip
+
+import (
+	"fmt"
+	"net"
+)
+
+type EmptyProvider struct{}
+
+func (e *EmptyProvider) Initialize() error {
+	return nil
+}
+func (e *EmptyProvider) GetGeoInfo(ip net.IP) (*GeoInfo, error) {
+	return nil, fmt.Errorf("you are using an empty GeoIP provider, please set a valid provider")
+}
+func (e *EmptyProvider) UpdateDatabase() error {
+	return fmt.Errorf("you are using an empty GeoIP provider, please set a valid provider")
+}
+func (e *EmptyProvider) Close() error {
+	return nil
+}
--- utils/geoip/geoip_test.go
+++ utils/geoip/geoip_test.go
@@ -10,7 +10,7 @@ import (
 
 // 测试GeoIP数据库的初始化和更新功能
 func TestInitGeoIp(t *testing.T) {
-	geoip.InitGeoIp()
+	geoip.CurrentProvider, _ = geoip.NewMaxMindGeoIPService()
 
 	// 检查数据库
 	fileInfo, err := os.Stat(geoip.GeoIpFilePath)
@@ -24,36 +24,36 @@ func TestInitGeoIp(t *testing.T) {
 	// IPv4
 	ipaddr := "8.8.8.8"
 	ip := net.ParseIP(ipaddr)
-	record, err := geoip.GetGeoIpInfo(ip)
+	record, err := geoip.CurrentProvider.GetGeoInfo(ip)
 	if err != nil {
 		t.Errorf("Failed to get GeoIP info for IP %s: %v", ipaddr, err)
 	}
 
 	if record != nil {
-		if record.Country.ISOCode == "" && record.Country.Names["zh-CN"] == "" {
+		if record.ISOCode == "" && record.Name == "" {
 			t.Errorf("Country information is missing for IP %s", ipaddr)
 		}
 	} else {
 		t.Errorf("GeoIP record is nil for IP %s", ipaddr)
 	}
 
-	t.Logf("IPv4:[%s]%s - %s", ipaddr, record.Country.ISOCode, record.Country.Names["zh-CN"])
+	t.Logf("IPv4:[%s]%s - %s", ipaddr, record.ISOCode, record.Name)
 
 	// IPv6
 	ipaddr = "2001:4860:4860::8888"
 	ip = net.ParseIP(ipaddr)
-	record, err = geoip.GetGeoIpInfo(ip)
+	record, err = geoip.CurrentProvider.GetGeoInfo(ip)
 	if err != nil {
 		t.Errorf("Failed to get GeoIP info for IPv6 %s: %v", ipaddr, err)
 	}
 	if record != nil {
-		if record.Country.ISOCode == "" && record.Country.Names["zh-CN"] == "" {
+		if record.ISOCode == "" && record.Name == "" {
 			t.Errorf("Country information is missing for IPv6 %s", ipaddr)
 		}
 	} else {
 		t.Errorf("GeoIP record is nil for IPv6 %s", ipaddr)
 	}
-	t.Logf("IPv6:[%s]%s - %s", ipaddr, record.Country.ISOCode, record.Country.Names["zh-CN"])
+	t.Logf("IPv6:[%s]%s - %s", ipaddr, record.ISOCode, record.Name)
 }
 
 func TestUnicodeEmoji(t *testing.T) {
